54ea5ac486c412c52edd34d858fab96490c3fff4
Tracyctng <23048617@myrp.edu.sg>
2025-11-23 23:56:08 +0800

    App.js

--- App.js
+++ App.js
@@ -1,17 +1,1 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-const styles = StyleSheet.create({
-  
-});
-
-const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Week 5 Exercise 4</Text>
-      <StatusBar translucent={false} />
-    </View>
-  );
-}
-
-export default App;
